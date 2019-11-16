0159ff6cba4dd3ca7611b3ccd775c5d459a7e888
samkahunga65 <samkahunga65@gmail.com>
2019-11-16 11:09:41 +0300

    Add files via upload

---
+++ game_logic.py
@@ -0,0 +1,12 @@
+import main
+
+print(' Welcome to poker joker, lets get started')
+
+while True:
+	#CREATE THE DECK
+	deck = Deck()
+	deck.shuffle()
+	table.append(deck.top_card)
+	print(f'the starting card is {table[0].__str__}')
+	# CREATE THE PLAYERS
+	 player_1 = 
